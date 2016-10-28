410c38a1895e931031a7afd0f77be54bfc17a032
Christopher Schultz <cschul25@uic.edu>
2016-10-28 14:10:03 -0500

    Working example

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # directory_reading_example
-example of how to use opendir, readdir, and closedir
+CS 361 - C reading contents of Directory
+
+Example of how to use opendir, readdir, and closedir.
+All code is in example.c. To compile, run "make" in terminal.
+
+Example of how to use the program:
+
+   CS 361 - C reading contents of Directory Example
+
+   Program reads in contents of a directory 
+      Terminal Input:
+        "./example test_dir"
+
+      Terminal Output:
+        Contents of test_dir are:
+        .
+        ..
+        test1.c
+        test2.txt
+        sub_dir
--- example.c
+++ example.c
@@ -3,19 +3,61 @@
 
    Program reads in contents of a directory and prints
    out contents to std out. Pass directory as argument
-   to this program. Output looks like:
+   to this program. Example:
+      Terminal Input:
+        "./example test_dir"
 
-      * Fill this in *
-
-
+      Terminal Output:
+        Contents of test_dir are:
+        .
+        ..
+        test1.c
+        test2.txt
+        sub_dir
   ------------------------------------------------
 */
-#include <errno.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+#define PATH_ARG 1
+#define DIRECTORY_LISTING_MAX_CHAR 1013 // can get overflow if have a large directory and not enough characters
 
+/*
+*
+* Note: Returns raw pointer to malloc'ed space. User is responsible
+* for freeing up this pointer after its use
+*
+*/
+char* get_directory_contents(char* directory_path)
+{
+  char* directory_listing = NULL;
+  
+  // open directory path up 
+  DIR* path = opendir(directory_path);
 
+  // check to see if opening up directory was successful
+  if(path != NULL)
+  {
+      directory_listing = (char*) malloc(sizeof(char)*DIRECTORY_LISTING_MAX_CHAR);
+      directory_listing[0] = '\0';
+
+      // stores underlying info of files and sub_directories of directory_path
+      struct dirent* underlying_file = NULL;
+
+      // iterate through all of the  underlying files of directory_path
+      while((underlying_file = readdir(path)) != NULL)
+      {
+          strcat(directory_listing, underlying_file->d_name);
+          strcat(directory_listing, "\n");
+      }
+      
+      closedir(path);
+  }
+
+  return directory_listing;
+}
 
 
 /*
@@ -29,10 +71,22 @@ int main(int argc, char** argv)
   //Check for enough arguments
   if(argc != 2)
   {
-    perror("Arguments to Program not correct. Exiting.\n");
+    fprintf(stderr, "Arguments to Program not correct. Exiting.\n");
     exit(-1);
   }
 
+  // read in contents and check for read in errors
+  char* directory_contents_str = get_directory_contents(argv[PATH_ARG]); 
+  if(directory_contents_str != NULL)
+  {
+    printf("Contents of %s are:\n", argv[PATH_ARG]);
+    printf("%s\n", directory_contents_str); 
+  }
+  else
+  {
+    fprintf(stderr, "Error reading contents of %s\n", argv[PATH_ARG]);
+    exit(-2);
+  }
 
   return 0;
 }
